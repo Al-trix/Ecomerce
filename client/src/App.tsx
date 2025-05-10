92f361995c069d8018e3d90ca9e8990861212efe
Al-trix <arboleda.alvaro2020@gmail.com>
2025-05-10 18:24:49 -0500

    update client

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,9 @@
 const App = () => {
   return (
-    <h1 className="">Hello world</h1>
+    <div>
+      <h1 className="text-2xl">Hello world</h1>
+    </div>
   )
 }
 
-export default App;
+export default App
