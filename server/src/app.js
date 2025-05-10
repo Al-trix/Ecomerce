7f20b65454f5be5e01feb4328da545885eb0e3d5
Al-trix <arboleda.alvaro2020@gmail.com>
2025-05-10 18:36:10 -0500

    add cors on server for use api in the client

--- server/src/app.js
+++ server/src/app.js
@@ -8,31 +8,27 @@ import ordersRoutes from './routes/api/orders.routes.js';
 import cartRoutes from './routes/api/cart.routes.js';
 import reviewsRoutes from './routes/api/reviews.routes.js';
 import cookieParser from 'cookie-parser';
-import path from 'path';
-import { fileURLToPath } from 'url';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+import cors from 'cors';
 
 //* creamos una nueva instancia de express
 const app = express();
 
 //? Middlewares
+app.use(
+  cors({
+    origin: 'http//:localhost:5173',
+  })
+);
 app.use(morgan('dev')); // * Morgan: nos permite ver las peticiones que llegan al servidor
 app.use(express.json()); // * Express.json: nos permite parsear el body de las peticiones a json
 app.use(cookieParser()); // * CookieParser: nos permite parsear las cookies de las peticiones
 
-//? Rutas 
+//? Rutas
 app.use(usersRoutes);
 app.use(sellersRoutes);
 app.use(productsRoutes);
 app.use(cartRoutes);
 app.use(ordersRoutes);
-app.use(reviewsRoutes)
-
-app.get('/chat', (req, res) => {
-  const filePath = path.resolve(__dirname, '../../client/index.html');
-  res.sendFile(filePath);
-});
+app.use(reviewsRoutes);
 
 export default app;
